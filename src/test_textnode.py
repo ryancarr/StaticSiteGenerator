a1f481705ba017e8c5a3dc9321e68cfe8fac990a
Ryan Carr <ryancarr@users.noreply.github.com>
2024-03-09 16:21:29 -0600

    Completed the first couple parts of the assignment

---
+++ src/test_textnode.py
@@ -0,0 +1,35 @@
+import unittest
+
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", text_type_bold)
+        node2 = TextNode("This is a text node", text_type_bold)
+        self.assertEqual(node, node2)
+    
+    def test_noteq(self):
+        node = TextNode('Testing', text_type_bold)
+        node2 = TextNode('Different', text_type_italic)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode('Testing', text_type_bold, 'https://wwww.google.com')
+        node2 = TextNode('Testing', text_type_bold)
+        self.assertNotEqual(node, node2)
+    
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual("TextNode(This is a text node, text, https://www.boot.dev)", repr(node))
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/textnode.py
@@ -0,0 +1,20 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __repr__(self) -> str:
+        return f'TextNode({self.text}, {self.text_type}, {self.url})'
+    
+    def __eq__(self, other: object) -> bool:
+        return (self.text == other.text
+                and self.text_type == other.text_type
+                and self.url == other.url)
